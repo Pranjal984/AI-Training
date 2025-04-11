6ecf0bc2590f4bbd7b36a1f1ef387cdd400a5cf9
Pranjal984 <pranjal.infomover@gmail.com>
2025-04-12 00:18:19 +0530

    wip

--- langchain_training/semantic_search.py
+++ langchain_training/semantic_search.py
@@ -5,6 +5,16 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.messages import HumanMessage, SystemMessage
 from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.vectorstores import InMemoryVectorStore
+from typing import List
+
+from langchain_core.documents import Document
+from langchain_core.runnables import chain
+
+
+
 load_dotenv()
 
 
@@ -14,7 +24,7 @@ if __name__ == "__main__":
     if not os.environ.get("MISTRAL_API_KEY"):
         os.environ["MISTRAL_API_KEY"] = getpass.getpass("Enter API key for Mistral AI: ")
 
-    file_path = "@TCS NQT MCQs @Hiringhustle 05b8be2e33ec40689dbc817afe5f7f25.pdf"
+    file_path = "nyse-nke-2023-10K-231100085.pdf"
     loader = PyPDFLoader(file_path)
     pdf_docs = loader.load()
     print(f"\n Loaded {len(pdf_docs)} pages from PDF.\n")
@@ -30,6 +40,37 @@ if __name__ == "__main__":
     #     print(f"--- Page {i} ---")
         # print(f"{docs[0].page_content[:200]}\n")
         # print(docs[0].metadata)
+
+
+
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+    # vector_1 = embeddings.embed_query(all_splits[0].page_content)
+    # vector_2 = embeddings.embed_query(all_splits[1].page_content)
+
+    # assert len(vector_1) == len(vector_2)
+    # print(f"Generated vectors of length {len(vector_1)}\n")
+    # print(vector_1[:10])
+
+
+
+    vector_store = InMemoryVectorStore(embeddings)
+    ids = vector_store.add_documents(documents=all_splits)
+    results = vector_store.similarity_search(
+        "How many distribution centers does Nike have in the US?"
+    )
+
+    print(results[0])
+
+
+
+    results = vector_store.similarity_search_with_score("What was Nike's revenue in 2023?")
+    doc, score = results[0]
+    print(f"Score: {score}\n")
+    print(doc)
+
+
+
 
 
 
